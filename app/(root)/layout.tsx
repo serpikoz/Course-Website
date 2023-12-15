196d302f7bbd34930f677f08351842d8aae89f66
Kadir <116947551+serpikoz@users.noreply.github.com>
2023-12-15 22:39:52 +0300

    add-first-page

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Js Mastery",
+  description: "JS Mastery Resources",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="min-h-screen bg-black-100 font-poppins">{children}</body>
+    </html>
+  );
+}
